f8394d8784df16c58e8dc7806fa703c3766ce30c
samuelguebo <samuelguebo@gmail.com>
2020-09-04 15:37:25 +0000

    [STYLE] Stats canva position, layout color blue -> salmon

--- js/stats.js
+++ js/stats.js
@@ -1,22 +1,22 @@
-const Stats  = {
-  
+const Stats = {
+
   displayChart: function (list) {
-    
+
     // Stats data
     list = this.percentile(list)
     //console.log(list)
     let data = {
       datasets: [{
         data: Object.values(list),
-        backgroundColor:[
+        backgroundColor: [
           "rgb(255, 99, 132)",
           "rgb(54, 162, 235)",
           "rgb(255, 205, 86)",
-          "#F24900","#FFBC20","#007bff","#55BA30","#FFA75B",
-          "#BC2FA0","#EE7600","#EE4000","#4EABFC","#DCC7AA",
-          "#E91E63","#F44336","#f18973","#2196F3","#3F51B5",
-          "#4CAF50","#FF9800","#FFC107","#9C27B0","#F44336"
-          ]
+          "#F24900", "#FFBC20", "#007bff", "#55BA30", "#FFA75B",
+          "#BC2FA0", "#EE7600", "#EE4000", "#4EABFC", "#DCC7AA",
+          "#E91E63", "#F44336", "#f18973", "#2196F3", "#3F51B5",
+          "#4CAF50", "#FF9800", "#FFC107", "#9C27B0", "#F44336"
+        ]
       }],
 
       labels: Object.keys(list),
@@ -25,9 +25,10 @@ const Stats  = {
     // For a pie chart
     let chartContainer = document.querySelector("#resultChart")
     let ctx = chartContainer.getContext('2d')
+    ctx.textAligh = 'left'
 
-    if(typeof resultChart === "undefined"){
-     resultChart = new Chart(ctx, {
+    if (typeof resultChart === "undefined") {
+      resultChart = new Chart(ctx, {
         type: 'doughnut',
         data: data,
         options: {
@@ -40,23 +41,23 @@ const Stats  = {
             }
           },
           legend: {
-              display: true,
-              position: 'right',
+            display: true,
+            position: 'left',
           }
         }
       });
 
     }
     // update
-    else{
+    else {
 
       resultChart.data.labels = Object.keys(list)
       resultChart.data.datasets[0].data = Object.values(list)
-      
+
       resultChart.update();
     }
-    
-    
+
+
   },
 
   /**
@@ -74,4 +75,3 @@ const Stats  = {
     }, {})
   }
 }
-
