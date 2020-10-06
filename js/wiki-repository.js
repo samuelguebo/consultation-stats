1acecaeaa7804701e71e20061fb4cbaab0092746
Agastya Chandrakant <acagastya@outlook.com>
2020-10-06 18:12:13 +0530

    [feat] show global rights

--- js/wiki-repository.js
+++ js/wiki-repository.js
@@ -139,6 +139,7 @@ const WikiRepository = {
      */
     getUserDetails: async (user) => {
         const relevantGroups = ['rollbacker', 'sysop', 'checkuser', 'oversighter', 'otrs members', 'stewards', 'staff'];
+        const relevantGlobalGroups = ['global-interface-editor', 'global-rollbacker', 'global-sysop', 'ombuds', 'otrs-member'];
         let wiki = document.querySelector('input[name=wiki]').value
         let username = encodeURIComponent(user.username)
         let query_url = "https://" + wiki + "/w/api.php?action=query&meta=globaluserinfo&format=json&guiuser="
@@ -158,6 +159,7 @@ const WikiRepository = {
                     user['registration'] = data.query.globaluserinfo.registration
                     user['home'] = data.query.globaluserinfo.home
                     user['rights'] = data.query.globaluserinfo.merged;
+                    user['globalGroups'] = data.query.globaluserinfo.groups;
                     user['editcount'] = data.query.globaluserinfo.editcount;
 
                     // Extract homewiki url
@@ -175,14 +177,26 @@ const WikiRepository = {
                     // Make sure users have a group and relevant groups
                     user['rights'] = user['rights'].filter(items => 'groups' in items && items.groups.some(r => relevantGroups.includes(r.toLowerCase())));
 
+                    // Only consider relevant global groups
+                    user['globalGroups'] = user['globalGroups'].filter(
+                        item => relevantGlobalGroups.includes(item)
+                    );
+
                     // sort by edit count
                     user['rights'] = user['rights'].sort((b, a) => a.editcount >= b.editcount);
 
-                    // Flatten results into comma separated list
+                    // Flatten results
                     user['rights'] = user['rights'].map(item => {
                         item['groups'] = item['groups'].filter(i => relevantGroups.indexOf(i) >= 0)
-                        return item['groups'].join(', ')
+                        return item['groups']
                     })
+
+                    // item['groups'] in the above line returns an array.  Faced errors when doing otherwise for fiwiki's User:Stryn
+                    if(Array.isArray(user.rights[0]))
+                        user['rights'] = user['rights'][0]
+
+                    // append global groups and convert to comma separated list
+                    user['rights'] = [...user['rights'], ...user['globalGroups']].join(', ')
 
                     return user
                 }
