32b192838ad549d20a2cc628ae33db62ac9c3e47
samuelguebo <samuelguebo@gmail.com>
2020-10-06 23:33:23 +0000

    [UI] Additional colors

--- js/stats.js
+++ js/stats.js
@@ -12,11 +12,14 @@ const Stats = {
       datasets: [{
         data: Object.values(list),
         backgroundColor: [
-          "#FF6385", "#36A2EB", "#FFCD56", "#F24900", "#FFBC20", "#007BFF",
-          "#55BA30", "#FFA75B", "#BC2FA0", "#EE7600", "#EE4000", "#4EABFC",
-          "#DCC7AA", "#E91E63", "#F44336", "#F18973", "#2196F3", "#3F51B5",
-          "#4CAF50", "#FF9800", "#FFC107", "#9C27B0", "#F44336", "#FDFFF0",
-          "#FFF8F0", "#FFF1F0", "#FFF0F8", "#F0F6FF"
+          "#FF6385", "#36A2EB", "#FFCD56", "#F24900",
+          "#FFBC20", "#007BFF", "#55BA30", "#FFA75B",
+          "#BC2FA0", "#EE7600", "#EE4000", "#4EABFC",
+          "#DCC7AA", "#E91E63", "#F44336", "#F18973",
+          "#2196F3", "#3F51B5", "#4CAF50", "#FF9800",
+          "#FFC107", "#9C27B0", "#F44336", "#FDFFF0",
+          "#FFF8F0", "#FFF1F0", "#FFF0F8", "#E0FFEE",
+          "#E0FFE6", "#FDFFF0", "#F7E0FF", "#F6FFE0"
         ]
       }],
 
