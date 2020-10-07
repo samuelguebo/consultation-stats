ec53c30de1bb74da2a2289df8e7141d1a0a9a356
Agastya Chandrakant <acagastya@outlook.com>
2020-10-07 13:26:55 +0530

    fix sorttable bug

--- js/sortable.js
+++ js/sortable.js
@@ -296,3 +296,106 @@ sorttable = {
 if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", sorttable.init, false);
 }
+if (/WebKit/i.test(navigator.userAgent)) {
+    var _timer = setInterval(function () {
+        if (/loaded|complete/.test(document.readyState)) {
+            sorttable.init();
+        }
+    }, 10);
+}
+window.onload = sorttable.init;
+function dean_addEvent(element, type, handler) {
+    if (element.addEventListener) {
+        element.addEventListener(type, handler, false);
+    }
+    else {
+        if (!handler.$$guid)
+            handler.$$guid = dean_addEvent.guid++;
+        if (!element.events)
+            element.events = {};
+        var handlers = element.events[type];
+        if (!handlers) {
+            handlers = element.events[type] = {};
+            if (element["on" + type]) {
+                handlers[0] = element["on" + type];
+            }
+        }
+        handlers[handler.$$guid] = handler;
+        element["on" + type] = handleEvent;
+    }
+}
+dean_addEvent.guid = 1;
+function removeEvent(element, type, handler) {
+    if (element.removeEventListener) {
+        element.removeEventListener(type, handler, false);
+    }
+    else {
+        if (element.events && element.events[type]) {
+            delete element.events[type][handler.$$guid];
+        }
+    }
+}
+function handleEvent(event) {
+    var returnValue = true;
+    event =
+        event ||
+            fixEvent(((this.ownerDocument || this.document || this).parentWindow || window)
+                .event);
+    var handlers = this.events[event.type];
+    for (var i in handlers) {
+        this.$$handleEvent = handlers[i];
+        if (this.$$handleEvent(event) === false) {
+            returnValue = false;
+        }
+    }
+    return returnValue;
+}
+function fixEvent(event) {
+    event.preventDefault = fixEvent.preventDefault;
+    event.stopPropagation = fixEvent.stopPropagation;
+    return event;
+}
+fixEvent.preventDefault = function () {
+    this.returnValue = false;
+};
+fixEvent.stopPropagation = function () {
+    this.cancelBubble = true;
+};
+if (!Array.forEach) {
+    Array.forEach = function (array, block, context) {
+        for (var i = 0; i < array.length; i++) {
+            block.call(context, array[i], i, array);
+        }
+    };
+}
+Function.prototype.forEach = function (object, block, context) {
+    for (var key in object) {
+        if (typeof this.prototype[key] == "undefined") {
+            block.call(context, object[key], key, object);
+        }
+    }
+};
+String.forEach = function (string, block, context) {
+    Array.forEach(string.split(""), function (chr, index) {
+        block.call(context, chr, index, string);
+    });
+};
+var forEach = function (object, block, context) {
+    if (object) {
+        var resolve = Object;
+        if (object instanceof Function) {
+            resolve = Function;
+        }
+        else if (object.forEach instanceof Function) {
+            object.forEach(block, context);
+            return;
+        }
+        else if (typeof object == "string") {
+            resolve = String;
+        }
+        else if (typeof object.length == "number") {
+            resolve = Array;
+        }
+        resolve.forEach(object, block, context);
+    }
+};
--- src/sortable.js
+++ src/sortable.js
@@ -380,3 +380,156 @@ if (document.addEventListener) {
 
 /* for Internet Explorer */
 /*@cc_on @*/
+/*@if (@_win32)
+    document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+    var script = document.getElementById("__ie_onload");
+    script.onreadystatechange = function() {
+        if (this.readyState == "complete") {
+            sorttable.init(); // call the onload handler
+        }
+    };
+/*@end @*/
+
+/* for Safari */
+if (/WebKit/i.test(navigator.userAgent)) {
+  // sniff
+  var _timer = setInterval(function () {
+    if (/loaded|complete/.test(document.readyState)) {
+      sorttable.init(); // call the onload handler
+    }
+  }, 10);
+}
+
+/* for other browsers */
+window.onload = sorttable.init;
+
+// written by Dean Edwards, 2005
+// with input from Tino Zijdel, Matthias Miller, Diego Perini
+
+// http://dean.edwards.name/weblog/2005/10/add-event/
+
+function dean_addEvent(element, type, handler) {
+  if (element.addEventListener) {
+    element.addEventListener(type, handler, false);
+  } else {
+    // assign each event handler a unique ID
+    if (!handler.$$guid) handler.$$guid = dean_addEvent.guid++;
+    // create a hash table of event types for the element
+    if (!element.events) element.events = {};
+    // create a hash table of event handlers for each element/event pair
+    var handlers = element.events[type];
+    if (!handlers) {
+      handlers = element.events[type] = {};
+      // store the existing event handler (if there is one)
+      if (element["on" + type]) {
+        handlers[0] = element["on" + type];
+      }
+    }
+    // store the event handler in the hash table
+    handlers[handler.$$guid] = handler;
+    // assign a global event handler to do all the work
+    element["on" + type] = handleEvent;
+  }
+}
+// a counter used to create unique IDs
+dean_addEvent.guid = 1;
+
+function removeEvent(element, type, handler) {
+  if (element.removeEventListener) {
+    element.removeEventListener(type, handler, false);
+  } else {
+    // delete the event handler from the hash table
+    if (element.events && element.events[type]) {
+      delete element.events[type][handler.$$guid];
+    }
+  }
+}
+
+function handleEvent(event) {
+  var returnValue = true;
+  // grab the event object (IE uses a global event object)
+  event =
+    event ||
+    fixEvent(
+      ((this.ownerDocument || this.document || this).parentWindow || window)
+        .event
+    );
+  // get a reference to the hash table of event handlers
+  var handlers = this.events[event.type];
+  // execute each event handler
+  for (var i in handlers) {
+    this.$$handleEvent = handlers[i];
+    if (this.$$handleEvent(event) === false) {
+      returnValue = false;
+    }
+  }
+  return returnValue;
+}
+
+function fixEvent(event) {
+  // add W3C standard event methods
+  event.preventDefault = fixEvent.preventDefault;
+  event.stopPropagation = fixEvent.stopPropagation;
+  return event;
+}
+fixEvent.preventDefault = function () {
+  this.returnValue = false;
+};
+fixEvent.stopPropagation = function () {
+  this.cancelBubble = true;
+};
+
+// Dean's forEach: http://dean.edwards.name/base/forEach.js
+/*
+	forEach, version 1.0
+	Copyright 2006, Dean Edwards
+	License: http://www.opensource.org/licenses/mit-license.php
+*/
+
+// array-like enumeration
+if (!Array.forEach) {
+  // mozilla already supports this
+  Array.forEach = function (array, block, context) {
+    for (var i = 0; i < array.length; i++) {
+      block.call(context, array[i], i, array);
+    }
+  };
+}
+
+// generic enumeration
+Function.prototype.forEach = function (object, block, context) {
+  for (var key in object) {
+    if (typeof this.prototype[key] == "undefined") {
+      block.call(context, object[key], key, object);
+    }
+  }
+};
+
+// character enumeration
+String.forEach = function (string, block, context) {
+  Array.forEach(string.split(""), function (chr, index) {
+    block.call(context, chr, index, string);
+  });
+};
+
+// globally resolve forEach enumeration
+var forEach = function (object, block, context) {
+  if (object) {
+    var resolve = Object; // default
+    if (object instanceof Function) {
+      // functions have a "length" property
+      resolve = Function;
+    } else if (object.forEach instanceof Function) {
+      // the object implements a custom forEach method so use that
+      object.forEach(block, context);
+      return;
+    } else if (typeof object == "string") {
+      // the object is a string
+      resolve = String;
+    } else if (typeof object.length == "number") {
+      // the object is array-like
+      resolve = Array;
+    }
+    resolve.forEach(object, block, context);
+  }
+};
