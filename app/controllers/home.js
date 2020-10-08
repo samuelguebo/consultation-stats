61cd0d1cf70011716df3e7e82bc4c88bb35f2e3e
Agastya Chandrakant <acagastya@outlook.com>
2020-10-08 13:36:04 +0530

    string array for path

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,7 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/../views/index.html"));
+  res.sendFile(path.join(__dirname, "/../views/index.html"));
 });
 
 module.exports = router;
