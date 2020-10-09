0bab7a1b01ca57d7e2d183b2b2bff8389f9ba779
samuelguebo <samuelguebo@gmail.com>
2020-10-09 00:31:04 +0000

    [IMPR] Update endpoint /users/:wikiDb/:pageId with actual data

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,25 +1,61 @@
 const express = require("express");
 const mysql = require("mysql");
 const settings = require("../settings");
+const validator = require("validator");
 const router = express.Router();
 
-const con = mysql.createConnection({
-  host: settings.host,
-  user: settings.user,
-  password: settings.password,
-  database: settings.database,
-});
+router.get("/users/:wikiDb/:pageId", function (req, res) {
+  try {
+    let wikiDb = req.params.wikiDb;
+    let pageId = req.params.pageId;
 
-router.get("/users/:wiki/:pageId", function (req, res) {
-  con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT * FROM offer", function (err, result, fields) {
-      if (err) throw err;
-      // console.log(result);
-      res.send(result);
+    // Initial sanitization, can be refined later
+    if (!validator.contains(wikiDb, "_p") || !validator.isNumeric(pageId)) {
+      throw false;
+    }
+
+    const con = mysql.createConnection({
+      host: settings.host,
+      user: settings.user,
+      password: settings.password,
+      database: wikiDb,
+      port: settings.port,
+      typeCast: function (field, next) {
+        if (field.type == "VAR_STRING") {
+          return field.string();
+        }
+        return next();
+      },
     });
-  });
-  //res.send(req.params)
+
+    con.connect(function (error) {
+      let sqlQuery = `
+      SELECT DISTINCT a.actor_name,
+      ( select count(*) from revision_userindex r
+      where r.rev_actor = a.actor_id
+      and r.rev_timestamp > (now() - INTERVAL 15 DAY + 0)
+      ) as total, p.page_id
+
+      FROM revision_userindex r
+      LEFT JOIN page p ON r.rev_page = p.page_id
+      LEFT JOIN actor a on r.rev_actor = a.actor_id
+      WHERE p.page_id = ? LIMIT 3;
+      `
+        .replace(/(\r\n|\n|\r)/gm, "")
+        .replace(/\s+/g, " ")
+        .trim();
+
+      con.query(sqlQuery, [pageId], function (error, result, fields) {
+        if (error) throw error;
+        res.send(result);
+      });
+    });
+    //res.send([wiki, pageId])
+  } catch (error) {
+    res.send({
+      error: error,
+    });
+  }
 });
 
 module.exports = router;
