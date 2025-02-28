95b2d478091bd83da84f23a0c4bbf08e21bb5fa7
Samuel Guebo <samuelguebo@gmail.com>
2025-02-28 00:37:50 -0700

    update: remove unecessary backend layer

--- app/controllers/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
-
-router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../views/index.html"));
-});
-
-module.exports = router;
